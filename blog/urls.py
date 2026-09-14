249486400eec16230c40609d6d2244850c1149e3
lindanz918-bit <lindanz918@gmail.com>
2026-09-14 20:48:26 +1200

    UPDATE ON CLASS

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from blog.views import home
+from blog.viewsets import PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+
+urlpatterns = [
+    path('', home, name='home'),
+
+] + router.urls
